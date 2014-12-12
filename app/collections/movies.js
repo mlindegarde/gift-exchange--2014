f6330d3825eb01c0dab4a25174682b425e7f372d
mlindegarde <mike.lindegarde@gmail.com>
2014-12-12 13:04:48 -0500

    Added code to shuffle the movie collection.

--- app/collections/movies.js
+++ app/collections/movies.js
@@ -3,11 +3,13 @@ var Movie = require('models/movie');
 
 var Movies = Backbone.Collection.extend({
     model: Movie,
+
     resetSelected: function() {
         this.each(function(model) {
             model.set({"selected": false});
         });
     },
+
     selectById: function(id) {
         this.resetSelected();
 
@@ -18,7 +20,27 @@ var Movies = Backbone.Collection.extend({
         });
 
         return movie.id;
+    },
+
+    randomizeOrder: function() {
+      //_suffleArray(this.collection);
+      this.collection.reset(this.collection.shuffle(), {silent: true});
+    },
+
+    /*
+    // Fisher-Yates shuffle
+    _suffleArray: function(array) {
+      for(var i = array.length-1; i > 0; i--) {
+        var j = Math.floor(Math.random()*(i+1));
+	var temp = array[i];
+
+	array[i] = array[j]
+	array[j] = temp;
+      }
+
+      return array;
     }
+    */
 });
 
 module.exports = Movies;
